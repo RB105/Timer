42768de3ffef5dbb841fec9b932556bceb31ad86
RB105 <01baxromrajabov@gmail.com>
2023-06-09 19:38:11 -0800

    timer added

---
+++ lib/view/time_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_timer_countdown/flutter_timer_countdown.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Timer"),),
+      body: Row(
+        children: [
+          const Text("Shuncha vaqt qoldi"),
+          TimerCountdown(
+            minutesDescription: "",
+            secondsDescription: "",
+            format: CountDownTimerFormat.minutesSeconds,
+            endTime: DateTime.now().add(const Duration(minutes: 1, seconds: 30)),
+            onEnd: () {
+              debugPrint("Hello");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
